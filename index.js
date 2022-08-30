8fb2c80e6c16b0a146b601a8bed21c567108c076
raz zarkor <razzarkor225@gmail.com>
2022-08-30 20:08:25 +0300

    v1

---
+++ index.js
@@ -0,0 +1,18 @@
+// import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
+
+export const handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from razzz! 123'),
+  };
+  return response;
+};
+
+// // For development/testing purposes
+// exports.handler = function(event) {
+//     const response = {
+//             statusCode: 200,
+//             body: JSON.stringify('Hello from studiocode! 123'),
+//            };
+//           return response;
+//   };
