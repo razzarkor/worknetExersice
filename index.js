c5fb7a412f350c0d4f3a961d1c3f198eb06fe8cf
raz zarkor <razzarkor225@gmail.com>
2022-08-31 20:32:21 +0300

    s

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from razzz! 1w222'),
+    body: JSON.stringify('Hello from razzz! 133'),
   };
   return response ;  
 };
