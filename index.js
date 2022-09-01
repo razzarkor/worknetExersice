59032219d73f5baedcd83117176bb92d5dd366d6
razest <112634391+razest@users.noreply.github.com>
2022-09-01 16:15:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('test from vss'),
+    body: JSON.stringify('test from another github acc'),
   };
   return response ;  
 };
